cf076f57ac5f183345a6dca32f8a809dede49f64
Stan Smith <choicedesign@gmail.com>
2022-02-20 13:21:39 -0700

    Add overall structure to the Default layout, header, main, footer

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,22 +1,45 @@
 <template>
-  <div class="layout">
-    <!--<header class="header">
+  <div class="">
+    <header class="">
       <strong>
         <g-link to="/">{{ $static.metadata.siteName }}</g-link>
       </strong>
       <nav class="nav">
         <g-link class="nav__link" to="/">Home</g-link>
-        <g-link class="nav__link" to="/about/">About</g-link>
+        <g-link class="nav__link" to="/products/">Products</g-link>
+        <!--<g-link class="nav__link" to="/about/">About</g-link>-->
       </nav>
-    </header>-->
-    <slot />
+    </header>
+
+    <main>
+      <slot />
+    </main>
+
+    <!--Footer-->
+    <footer>
+      <h4 class="h-3">Social</h4>
+      <ul>
+        <li v-for="account in $static.socialAccounts.edges" :key="account.id">
+          <a :href="account.node.url">{{ account.node.name }}</a>
+        </li>
+      </ul>
+    </footer>
   </div>
 </template>
 
-<static-query>
+<static-query type="graphql">
 query {
   metadata {
     siteName
+  }
+  socialAccounts: allContentfulSocialMediaAccount {
+    edges {
+      node {
+        id
+        name
+        url
+      }
+    }
   }
 }
 </static-query>
@@ -28,26 +51,5 @@ body {
   margin: 0;
   padding: 0;
   line-height: 1.5;
-}
-
-.layout {
-  display: flex;
-  height: 100vh;
-  max-width: 760px;
-  margin: 0 auto;
-  padding-left: 20px;
-  padding-right: 20px;
-}
-
-.header {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  margin-bottom: 20px;
-  height: 80px;
-}
-
-.nav__link {
-  margin-left: 20px;
 }
 </style>
